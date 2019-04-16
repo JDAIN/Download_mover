82923bc95d3239025f92127664b32d08ee238638
JD <poker98@web.de>
2019-04-16 13:28:46 +0200

    fixed error when attempting to pack and remove subfolder

--- downloadMover.py
+++ downloadMover.py
@@ -23,9 +23,10 @@ def rm_dup(path):
     md5_dict = defaultdict(list)
     for root, dirs, files in os.walk(path):  # the os.walk function allows checking subdirectories too...
         for filename in files:
-            print('\rReading: ' + filename, sep=' ', end='', flush=True)
             filepath = os.path.join(root, filename)
-            file_md5 = md5(filename)
+            print('\rReading: ' + filename, sep=' ', end='\t\t\t\t\t\t\t', flush=True)
+
+            file_md5 = md5(filepath)
             md5_dict[file_md5].append(filepath)
 
     for key in md5_dict:
@@ -33,44 +34,51 @@ def rm_dup(path):
         while len(file_list) > 1:
             item = file_list.pop()
             os.remove(item)
-            print('\rRemoved: ' + item, sep=' ', end='', flush=True)
+            print('\rRemoved: ' + item, sep=' ', end='\t\t\t\t\t\t\t', flush=True)
     print()
 
 
 def pack_zip(path):
-    #print(os.getcwd())
+    # print(os.getcwd())
     zipFileName = 'downloadsYYY_' + datetime.datetime.now().strftime('%d-%m-%Y_%H_%M') + '.zip'
-    print('Zipfilename: ' +zipFileName)
+    print('Zipfilename: ' + zipFileName + "\n")
     os.chdir(downloadpath_yyy)
-    #print(os.getcwd())
+    # print(os.getcwd())
     for files in os.listdir(downloadpath_yyy):
         # print(files)
-        # files = os.path.join(downloadpath_yyy, files)
+        files = os.path.join(downloadpath_yyy, files)
         # print(files)
         with zipfile.ZipFile(zipFileName, 'a', compression=zipfile.ZIP_DEFLATED) as down_zip:
             if 'downloadsYYY_' not in files:
-                print('\rPack to zip: '+files , sep=' ', end='', flush=True)
-                down_zip.write(files)
-                os.remove(files)
-    print()
+                print('\rPack to zip: ' + files, sep=' ', end='\t\t\t\t\t', flush=True)
+
+                if os.path.isdir(files):
+                    for f in os.listdir(files):
+                        down_zip.write(os.path.join(files, f))
+                        os.remove(os.path.join(files, f))
+
+                    os.rmdir(files)
+                else:
+                    down_zip.write(files)
+                    os.remove(files)
 
 
 
 if __name__ == '__main__':
-    #path for start/download_dir
+    # path for start/download_dir
     downloadpath_yyy = os.path.abspath(r'Y:\downloads')
-    #path for target dir
+    # path for target dir
     targetpath_zzz = os.path.abspath(r'Z:\Backup\downloadsBackup')
-    os.chdir(downloadpath_yyy) #changed cwd to download_dir
+    os.chdir(downloadpath_yyy)  # changed cwd to download_dir
     rm_dup(downloadpath_yyy)  # removes duplicates in download
     print('DONE REMOVING DUPLICATES')
 
-    #print(os.listdir(downloadpath_yyy))
-    pack_zip(downloadpath_yyy) # packs remaining files in zip
-    #print(os.getcwd())
-    print('DONE PACKING TO ZIP')
+    # print(os.listdir(downloadpath_yyy))
+    pack_zip(downloadpath_yyy)  # packs remaining files in zip
+    # print(os.getcwd())
+    print('\nDONE PACKING TO ZIP')
 
-    print('files to be moved to zzz: ' + str(os.listdir(downloadpath_yyy))) #lists to be moved files
+    print('files to be moved to zzz: ' + ', '.join(os.listdir(downloadpath_yyy)))  # lists to be moved files
     for file in os.listdir(downloadpath_yyy):
-        shutil.move(file, os.path.join(targetpath_zzz, file)) #moves zip and remaining files to backup
+        shutil.move(file, os.path.join(targetpath_zzz, file))  # moves zip and remaining files to backup
     print('DONE!')
